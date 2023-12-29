551e3d594d8b33751c2fd506a948aa183f2b5a7e
nihal <nihal.abedin.annon99@gmail.com>
2023-12-30 02:35:18 +0600

    playlist page

---
+++ .svelte-kit/types/src/routes/playlist/[id]/proxy+page.ts
@@ -0,0 +1,32 @@
+// @ts-nocheck
+import fetchRefresh from "$helpers/fetchRefresh";
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load = async ({ fetch: _fetch, params }: Parameters<PageLoad>[0]) => {
+    const fetch = (path: string) => fetchRefresh(_fetch, path)
+
+    const playlistRes = await fetch(`/api/spotify/playlists/${params.id}`);
+    if (!playlistRes.ok) {
+        throw error(playlistRes.status, "Failed to load playlists!")
+    }
+
+    const playlistJson: SpotifyApi.SinglePlaylistResponse = await playlistRes.json();
+
+    let color = null;
+    if (playlistJson.images.length > 0) {
+        const colorRes = await fetch(
+            `/api/average-color?${new URLSearchParams({
+                image: playlistJson.images[0].url
+            }).toString()}`
+        );
+        if (colorRes.ok) {
+            color = (await colorRes.json()).color;
+        }
+    }
+    return {
+        playlist: playlistJson,
+        color,
+        title: playlistJson.name
+    }
+}
